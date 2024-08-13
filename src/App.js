b60eb4efef8bb1d929a3c7bb6bc83b91527cdcb0
Sakshi Vij <sakshivij12@gmail.com>
2024-08-14 00:59:56 +0530

    update changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { FaFacebook } from 'react-icons/fa';
+import { SiGmail } from "react-icons/si";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="min-h-screen bg-[#111827] flex items-center justify-center">
+      <div class="bg-[#1F2937] p-8 rounded-lg shadow-lg w-full min-h-96 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+        <div class="flex space-x-4 overflow-x-auto">
+        <a href="https://facebook.com" className="text-red-600 hover:text-red-800 object-cover bg-white p-3">
+        <SiGmail size={30} />
+          </a>
+          
+          <a href="https://facebook.com" className="text-blue-600 hover:text-blue-800">
+            <FaFacebook size={30} />
+          </a>
+        </div>
+        {/* <h2 class="text-xl font-bold mb-4">Card Title</h2>
+    <p class="text-gray-700">This is a card sitting on top of the screen.</p> */}
+      </div>
     </div>
   );
 }
