e7bab663feec4efc972507cc00d9a379371df78c
Sakshi Vij <sakshivij12@gmail.com>
2024-08-16 02:20:18 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { FaFacebook } from 'react-icons/fa';
-import { SiGmail } from "react-icons/si";
 
 function App() {
   return (
-    <div class="min-h-screen bg-[#111827] flex items-center justify-center">
-      <div class="bg-[#1F2937] p-8 rounded-lg shadow-lg w-full min-h-96 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
+    <div class="min-h-screen bg-black flex items-center justify-center">
+      <div class="bg-white p-8 rounded-lg shadow-lg w-full min-h-96 max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl">
         <div class="flex space-x-4 overflow-x-auto">
-        <a href="https://facebook.com" className="text-red-600 hover:text-red-800 object-cover bg-white p-3">
-        <SiGmail size={30} />
+          {/* <a href="https://facebook.com" className="text-red-600 hover:text-red-800 object-cover bg-white p-2">
+            <SiGmail size={40} />
           </a>
-          
-          <a href="https://facebook.com" className="text-blue-600 hover:text-blue-800">
-            <FaFacebook size={30} />
+
+          <a href="https://facebook.com" className="text-blue-600 hover:text-blue-800 p-2 bg-white">
+            <FaLinkedin size={40} />
           </a>
-          <a href="https://facebook.com" className="text-blue-600 hover:text-blue-800">
-            <FaFacebook size={30} />
-          </a>
+          <a href="https://facebook.com" className="text-blue-600 hover:text-blue-800 p-2 bg-white">
+            <FaLinkedin size={40} />
+          </a> */}
+          <a href="https://x.com/sakshivij21"><img src="https://img.shields.io/badge/twitter-%2312100E.svg?&style=for-the-badge&logo=x&logoColor=white" alt="Twitter" /></a>
+          <a href="https://www.linkedin.com/in/sakshi-vij-b77058126/"><img src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn" /></a>
+          <a href="mailto:sakshivij12@gmail.com?subject=Hola%20Sakshi"><img src="https://img.shields.io/badge/gmail-%23D14836.svg?&style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail" /></a>
+          <a href="https://leetcode.com/sakshivij12/"><img src="https://img.shields.io/badge/leetcode-%2312100E.svg?&style=for-the-badge&logo=leetcode&logoColor=white" alt="Leetcode" /></a>
+          <a href="https://medium.com/@sakshivij12"><img src="https://img.shields.io/badge/medium-%2312100E.svg?&style=for-the-badge&logo=medium&logoColor=white" alt="Medium" /></a>
+
         </div>
         {/* <h2 class="text-xl font-bold mb-4">Card Title</h2>
     <p class="text-gray-700">This is a card sitting on top of the screen.</p> */}
